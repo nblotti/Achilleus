cf64d9a567d7589d27664032cdbc9003fbb22343
Nicholas Blotti <nblotti@gmail.com>
2020-07-01 22:16:06 +0200

    fix path

--- docker-resources/Base.R
+++ docker-resources/Base.R
@@ -1,5 +1,5 @@
 source_github <- function(strategyname, account) {
-  url <- paste('https://raw.githubusercontent.com/nblotti/@project.artifactId@/@project.version@/strategy/', strategyname, '.R', sep = "")
+  url <- paste('https://raw.githubusercontent.com/nblotti/@project.artifactId@/@project.artifactId@-@project.version@/strategy/', strategyname, '.R', sep = "")
   devtools::source_url(url)
 
   return(doOperation(account));
