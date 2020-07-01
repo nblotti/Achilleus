928fd4f741d67d1b011f4cb57410682f99ece5ca
Nicholas Blotti <nblotti@gmail.com>
2020-07-01 15:48:13 +0200

    script versionning

--- docker-resources/Base.R
+++ docker-resources/Base.R
@@ -1,5 +1,5 @@
 source_github <- function(strategyname) {
-  url <- paste('https://raw.githubusercontent.com/nblotti/R/master/strategy/', strategyname, '.R', sep = "")
+  url <- paste('https://raw.githubusercontent.com/nblotti/@project.artifactId@/@project.version@/strategy/', strategyname, '.R', sep = "")
   devtools::source_url(url)
 
 }
