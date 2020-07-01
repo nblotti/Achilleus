6574742c69faa803b93c645229bb1dbc39da8737
Nicholas Blotti <nblotti@gmail.com>
2020-07-01 15:38:09 +0200

    loading script starting

--- docker-resources/Base.R
+++ docker-resources/Base.R
@@ -1,5 +1,5 @@
 source_github <- function(strategyname) {
-  url <- paste('https://raw.githubusercontent.com/nblotti/R/master/strategy/', strategyname, '.R')
+  url <- paste('https://raw.githubusercontent.com/nblotti/R/master/strategy/', strategyname, '.R', sep = "")
   devtools::source_url(url)
 
 }
